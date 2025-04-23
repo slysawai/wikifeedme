3eac2f19ead003d0c6591fee9b13089e010f1fb3
Daniel Naber <naber@danielnaber.de>
2025-04-23 17:48:22 +0200

    use Plausible

--- php/analytics.php
+++ php/analytics.php
@@ -1,15 +1,1 @@
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(["trackPageView"]);
-  _paq.push(["enableLinkTracking"]);
-
-  (function() {
-    var u=(("https:" == document.location.protocol) ? "https" : "http") + "://openthesaurus.stats.mysnip-hosting.de/";
-    _paq.push(["setTrackerUrl", u+"piwik.php"]);
-    _paq.push(["setSiteId", "8"]);
-    var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-    g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Piwik Code -->
+<script defer data-domain="wikifeedme.openthesaurus.de" src="https://plausible.io/js/script.outbound-links.js"></script>
