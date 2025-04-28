a4f61fad024f8d7cc2e848595bd21aafd35785c6
Daniel Naber <naber@danielnaber.de>
2025-04-28 12:01:35 +0200

    cleanup - using PHP now for better deployability

--- djangosite/wikifeedme/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from wikifeedme import views
-
-urlpatterns = patterns('',
-                       url(r'^$', views.index),
-                       url(r'^feedMe/$', views.feedMe),
-                       url(r'^about/$', views.about),
-)
